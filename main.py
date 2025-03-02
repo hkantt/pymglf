7384b29cb98538e9dd5afbc09c569e6bb16d7a47
hkantt <hkantt.io@gmail.com>
2025-03-02 15:14:29 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 
 from states import *
 
+
+# Configure the window
+Window.set_gl_ver(3, 3)
+Window.set_size(1280, 720)
+Window.set_title("PyMGLF")
+
+# Initialize Core
 Core.init()
-Core.activate("RedState")
+
+# Set an initial state
+Core.activate("ExampleState")
+
+# Run the core (Always call Core.run() last)
 Core.run()
